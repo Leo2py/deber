1d7abb107b733d64dc71c7cad770c9eb7401a954
Leo2py <maojaramillo39@gmail.com>
2024-05-02 23:05:13 -0500

    Add files via upload

---
+++ deber1.py
@@ -0,0 +1,37 @@
+#Aplicar los conceptos aprendidos en Python:
+#La impresión en pantalla
+#Manejo de tipos de datos
+#Creación de variables y comentarios
+x = 8
+y = 4
+u = 7.5
+i = 4.8
+print(f"la suma de {x} y {y} es: {x+y}")
+print(f"La resta de {u} y {i} es: {u-i}")
+print("xxxxxxxxxxxxxxxxxxxxxxxxxx")
+#Multiplicacion
+print(f"La multiplicación de {x} y {y} es: {x*y}")
+#Division
+print(f"La División de {x} y {y} es: {x/y}")
+print("xxxxxxxxxxxxxxxxxxxxxxxxxx")
+#Manipulación de Cadenas de Texto
+name = "Mao Leonardo Tomala Leonardo"
+d = len(name)
+print(d)
+#La función "len" devuelve el número de valores de un elemento iterable
+print("xxxxxxxxxxxxxxxxxxxxx")
+#Conversión de Tipos de Datos
+t = 78
+print(t)
+print(type(t)) #tipo de dato original
+r = float(t)
+print(r)
+print(type(r)) #tipo de dato convertido en flotante
+print("xxxxxxxxxxxxxxxxxxxxx")
+g = 6.8
+print(g)
+print(type(g)) #tipo de dato original
+h = int(g)
+print(h)
+print(type(h)) #tipo de dato convertido en integer
+
